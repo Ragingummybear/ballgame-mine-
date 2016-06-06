cd5c2e9a74be06f06d870e8efeb0ce2394b0fee9
Ragingummybear <MilanS@Milans-MacBook-Air.local>
2016-06-05 19:55:46 -0700

    basic main method

--- BallGameAPCS/Startup.java
+++ BallGameAPCS/Startup.java
@@ -11,27 +11,15 @@ import java.awt.event.*;
  */
 public class Startup
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Startup
-     */
-    public Startup()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+   
+        public static void main(String [ ] args)
+{
+      player x = new player(100, 100, 100); //health, idk, other stats 
+          
+      x.begin();
+      
+      Karel.turnOff();
 }
+    }
+    
+
