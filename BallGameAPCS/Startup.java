774e1a1a0f0f6659eb9f93a831960fdb247aabca
Ragingummybear <MilanS@Milans-MacBook-Air.local>
2016-06-05 19:44:16 -0700

    main class
    nothing in it yet

---
+++ BallGameAPCS/Startup.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import javax.swing.JFrame;
+import java.awt.image.*;
+import java.awt.event.*;
+
+/**
+ * Write a description of class Startup here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Startup
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Startup
+     */
+    public Startup()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
