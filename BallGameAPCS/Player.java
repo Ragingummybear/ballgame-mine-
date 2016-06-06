06810de49b9c00cac9ae6c5b8189fa649b9061d3
harkmeister <harkeehalait@gmail.com>
2016-06-05 20:00:38 -0700

    +Player class

---
+++ BallGameAPCS/Player.java
@@ -0,0 +1,53 @@
+
+/**
+ * Describes player and player stats
+ */
+public class Player
+{
+    private int lvl;
+    private int attack;
+    private int hp;
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+        lvl=1;
+        attack= (int)(Math.pow(2.0,(double)lvl));
+        hp=75+(25*lvl);
+    }
+    /*
+     * Updates stats in case of a "level up"
+     */
+    public void levelUp()
+    {
+        System.out.println("You've leveled up! You are now level "+ ++lvl);
+        lvl++;
+        attack= (int)(Math.pow(2.0,(double)lvl));
+        hp=75+(25*lvl);
+        System.out.println("Your new stats read:");
+        System.out.println("Attack: "+ attack);
+        System.out.println("HP: "+hp);
+    }
+    /*
+     * Returns player level.
+     */
+    public int getLvl()
+    {
+        return lvl;
+    }
+     /*
+     * Returns player health.
+     */
+    public int getHP()
+    {
+        return hp;
+    }
+    /*
+     * Returns player attack.
+     */
+    public int getAttack()
+    {
+        return attack;
+    }
+}
