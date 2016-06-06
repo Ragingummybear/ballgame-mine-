31ebb8eec06cead8f3481ef9d0585b79f307fa5d
Ragingummybear <MilanS@Milans-MacBook-Air.local>
2016-06-05 18:20:01 -0700

    check to make sure commits are working

--- BallGameAPCS/Test.java
+++ BallGameAPCS/Test.java
@@ -5,7 +5,7 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Test
+public class Test //quick check
 {
     // instance variables - replace the example below with your own
     private int x;
